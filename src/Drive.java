6a2f297884a1d504e66b6b183e8f01211ba46832
ASUS <ASUS@DESKTOP-V3D7DIR>
2019-05-24 19:40:58 +0700

    Coba repost

---
+++ src/Drive.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author ASUS
+ */
+public class Drive {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int coba;
+        
+    }
+    
+}
